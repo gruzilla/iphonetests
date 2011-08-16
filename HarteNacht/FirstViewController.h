e2f2c83523a6133e031e97d482a5c87b57002e4b
Jakob Lahmer <jakob_l@gmx.at>
2011-08-16 18:55:25 +0200

    added camera

--- HarteNacht/FirstViewController.h
+++ HarteNacht/FirstViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import <MobileCoreServices/UTCoreTypes.h>
 
 @interface FirstViewController : UIViewController<UITextFieldDelegate> {
     UITextField *textInput;
--- HarteNacht/FirstViewController.m
+++ HarteNacht/FirstViewController.m
@@ -63,6 +63,13 @@
 }
 
 -(IBAction)buttonPressed:(id)sender {
+	
+	NSLog(@"Button pressed!");
+	
+	[self startCameraControllerFromViewController:self usingDelegate:self];
+	
+	/*
+
     //NSLog(@"Button Pressed!");
     self.name = textInput.text;
     
@@ -74,7 +81,135 @@
     NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@!", nameString];
     label.text = greeting;
     [greeting release];
+	 */
 }
 
 
+
+- (BOOL) startCameraControllerFromViewController: (UIViewController*) controller
+
+								   usingDelegate: (id <UIImagePickerControllerDelegate,
+												   
+												   UINavigationControllerDelegate>) delegate {
+	
+    if (([UIImagePickerController isSourceTypeAvailable:
+		  
+		  UIImagePickerControllerSourceTypeCamera] == NO)
+		
+		|| (delegate == nil)
+		
+		|| (controller == nil))
+		
+        return NO;
+	
+
+    UIImagePickerController *cameraUI = [[UIImagePickerController alloc] init];
+	
+    cameraUI.sourceType = UIImagePickerControllerSourceTypeCamera;
+	
+	cameraUI.cameraFlashMode = UIImagePickerControllerCameraFlashModeAuto;
+	
+    // Displays controls for video
+	cameraUI.mediaTypes = [[NSArray alloc] initWithObjects: (NSString *) kUTTypeMovie, nil];
+	
+	
+	// Hides the controls for moving & scaling pictures, or for
+	
+    // trimming movies. To instead show the controls, use YES.
+	
+    cameraUI.allowsEditing = NO;
+	
+	cameraUI.delegate = delegate;
+	
+	// show camera ui
+	[controller presentModalViewController: cameraUI animated: YES];
+	
+    return YES;
+	
+}
+
+// For responding to the user tapping Cancel.
+
+- (void) imagePickerControllerDidCancel: (UIImagePickerController *) picker {
+	
+	// remove from view
+    [[picker parentViewController] dismissModalViewControllerAnimated: YES];
+	
+    [picker release];
+	
+}
+
+
+
+// For responding to the user accepting a newly-captured picture or movie
+
+- (void) imagePickerController: (UIImagePickerController *) picker didFinishPickingMediaWithInfo: (NSDictionary *) info {
+	
+	
+	NSString *mediaType = [info objectForKey: UIImagePickerControllerMediaType];
+	
+    UIImage *originalImage, *editedImage, *imageToSave;
+	
+    // Handle a still image capture
+	
+    if (CFStringCompare ((CFStringRef) mediaType, kUTTypeImage, 0)
+		
+		== kCFCompareEqualTo) {
+		
+		
+		
+        editedImage = (UIImage *) [info objectForKey:
+								   
+								   UIImagePickerControllerEditedImage];
+		
+        originalImage = (UIImage *) [info objectForKey:
+									 
+									 UIImagePickerControllerOriginalImage];
+		
+		
+		
+        if (editedImage) {
+			
+            imageToSave = editedImage;
+			
+        } else {
+			
+            imageToSave = originalImage;
+			
+        }
+		
+		
+		
+		// Save the new image (original or edited) to the Camera Roll
+        UIImageWriteToSavedPhotosAlbum (imageToSave, nil, nil , nil);
+		
+    }
+	
+	
+	
+    // Handle a movie capture
+    if (CFStringCompare ((CFStringRef) mediaType, kUTTypeMovie, 0) == kCFCompareEqualTo) {
+
+        NSString *moviePath = [[info objectForKey:UIImagePickerControllerMediaURL] path];
+		
+		// save in photo album
+        if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum (moviePath)) {
+			
+            UISaveVideoAtPathToSavedPhotosAlbum (moviePath, nil, nil, nil);
+		}
+		
+		// @TODO
+		// upload to page
+    }
+	
+	// hide camera ui
+    [[picker parentViewController] dismissModalViewControllerAnimated: YES];
+    [picker release];
+	
+}
+
+
+
 @end
+
+
